ab80df35448ddd0cb0a8c3569531e47a03709235
Aníbal Páez Gallego <124251900+Anibalpaezz@users.noreply.github.com>
2026-08-07 17:29:13 +0200

    feat(resume): remove CAPTCHA from CV download - download button now links directly to the PDF

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,9 +3,4 @@ declare interface Window {
   __lang: string;
   __setLang: (lang: string) => void;
   __toggleMenu: () => void;
-  __openCaptcha: () => void;
-  __closeCaptcha: () => void;
-  __refreshCaptcha: () => void;
-  __verifyCaptcha: () => void;
-  __triggerDownload: () => Promise<void>;
 }
